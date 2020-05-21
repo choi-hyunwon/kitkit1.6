222e6870cbf671ab7763b13c3b88d10d6db43a9e
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-05-21 17:02:50 +0900

    myui-vuejs 세팅 - package.json / nuxt.config.js / README.md 수정 - .gitignore 추가

---
+++ client/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <fragment>
+    <IntroductionSection/>
+    <div class="main-bg">
+      <div class="bar left-bar"></div>
+      <div class="bar right-bar"></div>
+      <img class="main-bg" src="~@/assets/img/main/main-bg.png" alt="My UI background image" />
+    </div>
+    <div class="bg-container">
+      <SupportSection/>
+    </div>
+    <ComponentSection/>
+  </fragment>
+</template>
+
+<script>
+  import ComponentSection from '../components/index/ComponentSection'
+  import IntroductionSection from '../components/index/IntroductionSection'
+  import SupportSection from '../components/index/SupportSection'
+
+  export default {
+    layout : 'default',
+    components: {
+      ComponentSection,
+      IntroductionSection,
+      SupportSection
+    }
+  }
+</script>
+
+
