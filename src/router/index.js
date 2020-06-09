9ce9216b1a1cca820e84c874c55ac12f6c496471
bohyeonp <219031@naver.com>
2020-06-09 10:32:36 +0900

    구조 정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,11 @@
 // pages
-import Index from './../views/pages/Index.vue'
-import Menu from './../views/pages/doc/_menu.vue'
 import Login from './../views/pages/Login.vue'
 import Detail from './../views/pages/Detail.vue'
 
 export const routes = [
     {
-        name: 'index',
-        path: '/',
-        component: Index
-    },
-    {
-        name: 'menu',
-        path: '/doc/:menu',
-        component: Menu
-    },
-    {
         name: 'login',
-        path: '/login',
+        path: '/',
         component: Login
     },
     {
