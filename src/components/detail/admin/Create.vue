c3ecae9b214a80d6150ee01c14300ddf0eaba2a7
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-06-14 19:09:08 +0900

    - Create 페이지  -> input, label 영역 좌우 패딩  -> input required 추가  -> 툴팁 -> Number of license 2줄 (텍스트 수정 필요) - Aside -> 높이 수정

--- src/components/detail/admin/Create.vue
+++ src/components/detail/admin/Create.vue
@@ -7,68 +7,88 @@
             <!-- Account Information -->
             <div class="category accountInfo">
                 <div class="font-weight-bold subtitle">Account Information</div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">ID</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter ID">
+                <div class="list">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">ID *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control is-invalid" title="ID" placeholder="Please Enter ID" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter ID.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col item" style="padding-right: 60px;">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Password *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Password" placeholder="Please Enter Password" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Password.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col item" style="padding-right: 60px;">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Password</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Password">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Name *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Name" placeholder="Please Enter Name" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Name.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col item" style="padding-right: 60px;">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Email Address *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Email Address" placeholder="Please Enter Email Address" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Email Address.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Name</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Name">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Organization *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Organization" placeholder="Please Enter Organization" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Organization.
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col item" style="padding-right: 60px;">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Email Address</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Email Address">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Organization</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Organization">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col item" style="padding-right: 60px;">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Organization Type</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Organization Type">
+                        <div class="col item" style="padding-right: 60px;">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Organization Type *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Organization Type" placeholder="Please Enter Organization Type" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Organization Type.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -78,85 +98,99 @@
             <!-- User Information -->
             <div class="category userInfo">
                 <div class="font-weight-bold subtitle">User Information</div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Country</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Country">
+                <div class="list">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Country *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control is-invalid" title="Country" placeholder="Please Enter Country" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Country.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col item" style="padding-right: 60px;">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">City/Province *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="City/Province" placeholder="Please Enter City/Province" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter City/Province.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col item" style="padding-right: 60px;">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">City/Province</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter City/Province">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Number Of license *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Number Of license" placeholder="Please Enter Number Of license" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Number Of license.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col item" style="padding-right: 60px;">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Expiration date *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Expiration date" placeholder="Please Enter Expiration date" required>
+                                    <div class="invalid-feedback">
+                                        This is required. Please enter Expiration date.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Number Of license</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Number Of license">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Product *</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <select class="custom-select" title="Product">
+                                        <option value="1" selected>English and Math</option>
+                                        <option value="2">Swahili and Math</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
+                        <div class="col item"></div>
                     </div>
-                    <div class="col item" style="padding-right: 60px;">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Expiration date</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Expiration date">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Product</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <select class="custom-select" title="Text">
-                                    <option selected>Normal</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col item"></div>
                 </div>
             </div>
 
             <!-- Internal -->
             <div class="category internalInfo">
                 <div class="font-weight-bold subtitle">Internal</div>
-                <div class="row">
-                    <div class="col item">
-                        <div class="form-group row">
-                            <label class="col col-form-label">
-                                <span class="col-label-text">Staff Name</span>
-                            </label>
-                            <div class="col col-form-input">
-                                <input type="text" class="form-control" title="example" placeholder="Please Enter Staff Name">
+                <div class="list">
+                    <div class="row">
+                        <div class="col item">
+                            <div class="form-group row">
+                                <label class="col col-form-label">
+                                    <span class="col-label-text">Staff Name</span>
+                                </label>
+                                <div class="col col-form-input">
+                                    <input type="text" class="form-control" title="Staff Name" placeholder="Please Enter Staff Name">
+                                </div>
                             </div>
                         </div>
+                        <div class="col item"></div>
                     </div>
-                    <div class="col item"></div>
                 </div>
             </div>
 
@@ -194,25 +228,29 @@
     .createAccountForm .category .subtitle{
         margin-bottom: 30px;
     }
-    .createAccountForm .category .item {
+    .createAccountForm .category .list {
+        padding: 0 60px;
+    }
+    .createAccountForm .category .list .item {
         margin: 0 auto 30px;
         height: 60px;
         min-width: 680px;
         max-width: 830px;
     }
-    .createAccountForm .category .item:first-child {
+    .createAccountForm .category .list .item:first-child {
     }
-    .createAccountForm .category .item .col-form-label {
+    .createAccountForm .category .list .item .col-form-label {
         width: 250px;
         min-width: 250px;
         max-width: 250px;
         padding: 18px 0;
     }
-    .createAccountForm .category .item .col-form-label .col-label-text {
+    .createAccountForm .category .list .item .col-form-label .col-label-text {
         font-size: 24px;
         line-height: 24px;
+        color: #666;
     }
-    .createAccountForm .category .item .col-form-input {
+    .createAccountForm .category .list .item .col-form-input {
         width: 100%;
         min-width: 390px;
         max-width: 390px;
