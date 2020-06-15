c87a99276651423a42ca99763a41fdb49c5ec55b
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-06-15 17:01:29 +0900

    페이지네이션 삭제

--- src/components/detail/AgGrid.vue
+++ src/components/detail/AgGrid.vue
@@ -17,40 +17,6 @@
                      :rowData="rowData">
         </ag-grid-vue>
 
-        <div class="pages">
-            <ul class="pagination pagination-circle">
-                    <li class="page-item disabled">
-                        <a class="page-link page-arrow" aria-label="Previous">
-                            <span aria-hidden="true"><font-awesome-icon :icon="['fas', 'angle-double-left']"/></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <li class="page-item disabled">
-                        <a class="page-link page-arrow" aria-label="Previous">
-                            <span aria-hidden="true"><font-awesome-icon :icon="['fas', 'chevron-left']"/></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <li class="page-item active"><a class="page-link">1</a></li>
-                    <li class="page-item"><a class="page-link">2</a></li>
-                    <li class="page-item"><a class="page-link">3</a></li>
-                    <li class="page-item"><a class="page-link">4</a></li>
-                    <li class="page-item"><a class="page-link">5</a></li>
-                    <li class="page-item">
-                        <a class="page-link page-arrow" aria-label="Next">
-                            <span aria-hidden="true"><font-awesome-icon :icon="['fas', 'chevron-right']"/></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link page-arrow" aria-label="Next">
-                            <span aria-hidden="true"><font-awesome-icon :icon="['fas', 'angle-double-right']"/></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                </ul>
-        </div>
-
     </div>
 </template>
 
