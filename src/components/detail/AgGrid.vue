a7ec634bd9cfec01acabf75927dbdb10399075d8
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-06-25 23:08:31 +0900

    Dashboard 열간격 조정

--- src/components/detail/AgGrid.vue
+++ src/components/detail/AgGrid.vue
@@ -76,7 +76,7 @@
 
             if(this.status === 'user'){
                 this.columnDefs = [
-                    {headerName: 'Tablet No.', field: 'tabletNO', pinned: 'left', sort : 'asc',
+                    {headerName: 'Tablet No.', field: 'tabletNO', pinned: 'left', sort : 'asc', width: 130,
                         comparator : (a, b) => {
                             if(a === "" && b !== "") return -1;
                             else if(a!== "" && b === "") return 1;
@@ -90,13 +90,13 @@
                             }
                         }
                     },
-                    {headerName: 'First Name', field: 'firstName', pinned: 'left',
+                    {headerName: 'First Name', field: 'firstName', pinned: 'left', width: 130,
                         cellRenderer: function(params) {
                             return '<a href="https://api.kitkitschool.com/account/downloadCSV?sessionId=' + sessionID + '&userID=' + params.data.userID + '" target="_blank">'+ params.value+'</a>'
                         }
                     },
-                    {headerName: 'Last Name', field: 'lastName'},
-                    {headerName: 'Last Update', field: 'lastUpdate',filter: 'agDateColumnFilter',
+                    {headerName: 'Last Name', field: 'lastName', width: 130},
+                    {headerName: 'Last Update', field: 'lastUpdate',filter: 'agDateColumnFilter', width: 155,
                         filterParams: {
                             comparator: (filterLocalDateAtMidnight, cellValue) => {
                                 let dateAsString = cellValue;
@@ -122,35 +122,35 @@
                         }
                     },
                     {headerName: 'ID', field: 'userID', hide: "true"},
-                    {headerName: 'Grade', field: 'grade',filter: 'agNumberColumnFilter'},
-                    {headerName: 'Class', field: 'class'},
-                    {headerName: 'First Played', field: 'startTime',filter: 'agDateColumnFilter'},
-                    {headerName: 'Last Played', field: 'endTime',filter: 'agDateColumnFilter'},
-                    {headerName: 'Play Time (Total min.)', field: 'playTimeCount',filter: 'agNumberColumnFilter'},
+                    {headerName: 'Grade', field: 'grade',filter: 'agNumberColumnFilter', width: 130},
+                    {headerName: 'Class', field: 'class', width: 130},
+                    {headerName: 'First Played', field: 'startTime',filter: 'agDateColumnFilter', width: 140},
+                    {headerName: 'Last Played', field: 'endTime',filter: 'agDateColumnFilter', width: 140},
+                    {headerName: 'Play Time (Total min.)', field: 'playTimeCount',filter: 'agNumberColumnFilter', width: 175},
                     {headerName: 'Literacy Course', groupId : 'progressLGroup',
                         children : [
-                            {headerName: 'Egg', field: 'progressLEgg',filter: 'agNumberColumnFilter'},
-                            {headerName: 'Day', field: 'progressLDay',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Egg', field: 'progressLEgg',filter: 'agNumberColumnFilter', width: 110},
+                            {headerName: 'Day', field: 'progressLDay',filter: 'agNumberColumnFilter', width: 110}]},
                     {headerName: 'Math Course', groupId : 'progressMGroup',
                         children : [
-                            {headerName: 'Egg', field: 'progressMEgg',filter: 'agNumberColumnFilter'},
-                            {headerName: 'Day', field: 'progressMDay',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Egg', field: 'progressMEgg',filter: 'agNumberColumnFilter', width: 110},
+                            {headerName: 'Day', field: 'progressMDay',filter: 'agNumberColumnFilter', width: 110}]},
                     {headerName: 'Pre-test Literacy', groupId : 'preTestLGroup',
                         children : [
-                            {headerName: 'Test Date', field: 'PretestLDate',filter: 'agDateColumnFilter'},
-                            {headerName: 'Score', field: 'PretestLScore',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Test Date', field: 'PretestLDate',filter: 'agDateColumnFilter', width: 120},
+                            {headerName: 'Score', field: 'PretestLScore',filter: 'agNumberColumnFilter', width: 125}]},
                     {headerName: 'Pre-test Math', groupId : 'preTestMGroup',
                         children : [
-                            {headerName: 'Test Date', field: 'PretestMDate',filter: 'agDateColumnFilter'},
-                            {headerName: 'Score', field: 'PretestMScore',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Test Date', field: 'PretestMDate',filter: 'agDateColumnFilter', width: 120},
+                            {headerName: 'Score', field: 'PretestMScore',filter: 'agNumberColumnFilter', width: 125}]},
                     {headerName: 'Post-test Literacy', groupId : 'postTestLGroup',
                         children : [
-                            {headerName: 'Test Date', field: 'posttestLDate',filter: 'agDateColumnFilter'},
-                            {headerName: 'Score', field: 'posttestLScore',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Test Date', field: 'posttestLDate',filter: 'agDateColumnFilter', width: 120},
+                            {headerName: 'Score', field: 'posttestLScore',filter: 'agNumberColumnFilter', width: 125}]},
                     {headerName: 'Post-test Math', groupId : 'postTestMGroup',
                         children : [
-                            {headerName: 'Test Date', field: 'posttestMDate',filter: 'agDateColumnFilter'},
-                            {headerName: 'Score', field: 'posttestMScore',filter: 'agNumberColumnFilter'}]},
+                            {headerName: 'Test Date', field: 'posttestMDate',filter: 'agDateColumnFilter', width: 120},
+                            {headerName: 'Score', field: 'posttestMScore',filter: 'agNumberColumnFilter', width: 125}]},
                 ];
             }else if (this.status === 'admin'){
                 this.columnDefs = [
@@ -166,7 +166,7 @@
                     {headerName: 'Product', field: 'productType', width: 190},
                     {headerName: 'Expiration Date', field: 'expdate',filter: 'agDateColumnFilter', width: 165},
                     {headerName: 'Staff Name', field: 'contactName', width: 130},
-                    {headerName: 'Last Update', field: 'lastUpdate',filter: 'agDateColumnFilter', width: 145,
+                    {headerName: 'Last Update', field: 'lastUpdate',filter: 'agDateColumnFilter', width: 155,
                         filterParams: {
                             comparator: (filterLocalDateAtMidnight, cellValue) => {
                                 let dateAsString = cellValue;
@@ -387,6 +387,14 @@
 
     .ag-sort-order {
         display: none;
+    }
+
+    .ag-theme-alpine .ag-cell a {
+        font-size: 20px;
+        line-height: 24px;
+        color: #878d99;
+        padding-top: 17px;
+        padding-bottom: 17px;
     }
 
     .ag-theme-alpine .ag-cell a:hover {
